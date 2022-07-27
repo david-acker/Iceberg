a650fe3d9407bdf18ce8824fe88990dd82b3cddd
David Acker <davidacker3@gmail.com>
2022-07-27 19:28:05 -0400

    Fix argument validation

--- src/CommandLine/src/Commands/MapCommand.cs
+++ src/CommandLine/src/Commands/MapCommand.cs
@@ -313,8 +313,8 @@ internal class MapCommand : ICommand
 
                 var maximumDistanceString = result.Tokens.Single().Value;
 
-                if (!string.IsNullOrEmpty(maximumDistanceString)
-                    || int.TryParse(maximumDistanceString, out int maximumDistance))
+                if (string.IsNullOrEmpty(maximumDistanceString)
+                    || !int.TryParse(maximumDistanceString, out int maximumDistance))
                 {
                     result.ErrorMessage = "The maximum distance must be a valid integer greater than zero.";
                     return null;
